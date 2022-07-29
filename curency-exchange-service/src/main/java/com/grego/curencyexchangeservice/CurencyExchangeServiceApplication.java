a0b409f743f828016570a95122a3cd2a39f9fc7c
Grego Garcia <82552333+Dgarciadani@users.noreply.github.com>
2022-07-29 01:07:40 -0300

    [ADD] currency exchange microservices

---
+++ curency-exchange-service/src/main/java/com/grego/curencyexchangeservice/CurencyExchangeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.grego.curencyexchangeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurencyExchangeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurencyExchangeServiceApplication.class, args);
+	}
+
+}
