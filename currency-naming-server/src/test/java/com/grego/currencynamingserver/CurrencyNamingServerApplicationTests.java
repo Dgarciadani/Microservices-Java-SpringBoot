795b76aeb388e083bcb0637454a80c3c18bd8bc9
Grego Garcia <82552333+Dgarciadani@users.noreply.github.com>
2022-08-03 17:51:50 -0300

    [ADD] Eureka naming server

---
+++ currency-naming-server/src/test/java/com/grego/currencynamingserver/CurrencyNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grego.currencynamingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
