98fbe29282d3cb2254b12720390a2f1243489eb4
Grego Garcia <82552333+Dgarciadani@users.noreply.github.com>
2022-08-23 17:53:13 -0300

    [ADD] Bulkhead config

--- currency-exchange-service/src/main/java/com/grego/currencyexchangeservice/controller/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/grego/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -1,6 +1,8 @@
 package com.grego.currencyexchangeservice.controller;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,7 +23,15 @@ public class CircuitBreakerController {
     // by default the method try 3 times
 
 
+   // @RateLimiter(name = "default")
+    //10s => 10000 calls to the api
+
+
+    @Bulkhead(name = "default")
+
+
     // the circuit breaker no call the method if the service is down directly call the fallback method
+    // naming with a personalized name
     @CircuitBreaker(name = "default", fallbackMethod = "hardcodedResponse")
     public String sampleApi() {
         logger.info("Sample API Call");
